c1d6b31a1a67bdbc587e93725c65125633fea143
aridwia <aridwiananto@gmail.com>
2017-10-12 14:45:18 +0700

    buat controllers articles

---
+++ server/controllers/controllerArticles.js
@@ -0,0 +1,63 @@
+const Articles = require('../models/Article');
+
+var getallArticles = (req,res) => {
+  Articles.find({})
+  .then(allArticles => {
+    res.send(allArticles)
+  })
+  .catch(err => {
+    res.send(err)
+  })
+}
+
+var createArticle = (req,res) => {
+  Articles.create({
+    title: req.body.title,
+    content: req.body.content,
+    category: req.body.category,
+    author: req.body.author
+  })
+  .then(newArticle => {
+    res.send(newArticle)
+  })
+  .catch(err => {
+    res.send(err)
+  })
+}
+
+var updateArticle = (req,res) => {
+  Articles.updateOne({_id: req.params.id},{
+    title: req.body.title,
+    content: req.body.content,
+    category: req.body.category,
+    author: req.body.author
+  })
+  .then(updatedArticle => {
+    res.send(updatedArticle)
+  })
+  .catch(err => {
+    res.send(err)
+  })
+}
+
+var removeArticle = (req,res) => {
+  Articles.deleteOne({_id: req.params.id})
+  .then(deletedArticle => {
+    res.send(deletedArticle)
+  })
+  .catch(err => {
+    res.send(err)
+  })
+}
+
+var getoneArticle = (req,res) => {
+  Articles.findOne({_id:req.params.id})
+  .then(oneArticle => {
+    res.send(oneArticle)
+  })
+  .catch(err => {
+    res.send(err)
+  })
+}
+
+module.exports = {getallArticles,createArticle,updateArticle,removeArticle,getoneArticle};
