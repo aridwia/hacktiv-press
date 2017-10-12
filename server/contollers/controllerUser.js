85690cd1b53d6168f301c43b85556dd1671d6ef1
aridwia <aridwiananto@gmail.com>
2017-10-12 13:53:10 +0700

    add bcryptjs dan controller user

---
+++ server/contollers/controllerUser.js
@@ -0,0 +1,57 @@
+const User = require('../models/User')
+var bcrypt = require('bcryptjs');
+var salt = bcrypt.genSaltSync(10);
+
+var getallUser = (req,res) => {
+  User.find({})
+  .then(allUser => {
+    res.send(allUser)
+  })
+  .catch(err => {
+    res.send(err)
+  })
+}
+
+var createUser = (req,res) => {
+  let hash = bcrypt.hashSync(req.body.password,salt)
+  User.create({
+    username: req.body.username
+    password: hash
+    email: req.body.email
+    fullname: req.body.fullname
+  })
+  .then(newUser => {
+    res.send(newUser)
+  })
+  .catch(err => {
+    res.send(err)
+  })
+}
+
+var updateUser = (req,res) => {
+  let hash = bcrypt.hashSync(req.body.password,salt)
+  User.updateone({_id: req.params.id},{
+    username: req.body.username
+    password: hash
+    email: req.body.email
+    fullname: req.body.fullname
+  })
+  .then(updateUser => {
+    res.send(updateUser)
+  })
+  .catch(err => {
+    res.send(err)
+  })
+}
+
+var removeUser = (req,res) => {
+  User.deleteOne({_id: req.params.id})
+  .then(removeUser => {
+    res.send(removeUser)
+  })
+  .catch(err => {
+    res.send(err)
+  })
+}
+
+module.exports = {getallUser,createUser,updateUser,removeUser};
